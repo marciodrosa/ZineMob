4d6d42e35577e9f3f09e1a5b6026de4bd01f12bd
Márcio Daniel da Rosa <marciodrosa@gmail.com>
2012-02-05 01:17:17 -0200

    Create rounded corners on RectangleElement.

--- ZineMob/src/com/zine/zinemob/drawableelement/RectangleElement.java
+++ ZineMob/src/com/zine/zinemob/drawableelement/RectangleElement.java
@@ -9,13 +9,22 @@ public class RectangleElement extends DrawableElement {
 	
 	private Color color = new Color(0xff000000);
 	private boolean fill = true;
+	private int roundedCornerH, roundedCornerV;
 
 	protected void drawElement(Graphics graphics) {
 		graphics.setColor(this.color.getComponents());
-		if (fill) {
-			graphics.fillRect(0, 0, getWidth(), getHeight());
+		if (roundedCornerH == 0 && roundedCornerV == 0) {
+			if (fill) {
+				graphics.fillRect(0, 0, getWidth(), getHeight());
+			} else {
+				graphics.drawRect(0, 0, getWidth(), getHeight());
+			}
 		} else {
-			graphics.drawRect(0, 0, getWidth(), getHeight());
+			if (fill) {
+				graphics.fillRoundRect(0, 0, getWidth(), getHeight(), roundedCornerH, roundedCornerV);
+			} else {
+				graphics.drawRoundRect(0, 0, getWidth(), getHeight(), roundedCornerH, roundedCornerV);
+			}
 		}
 	}
 
@@ -47,5 +56,27 @@ public class RectangleElement extends DrawableElement {
 	 */
 	public void setFill(boolean fill) {
 		this.fill = fill;
+	}
+
+	/**
+	 * Returns the size of the rounded horizontal corner, in pixels.
+	 */
+	public int getRoundedCornerH() {
+		return roundedCornerH;
+	}
+
+	/**
+	 * Returns the size of the rounded vertical corner, in pixels.
+	 */
+	public int getRoundedCornerV() {
+		return roundedCornerV;
+	}
+
+	/**
+	 * Sets the size of the rounded corner, in pixels.
+	 */
+	public void setRoundedCorner(int roundedCornerH, int roundedCornerV) {
+		this.roundedCornerH = roundedCornerH;
+		this.roundedCornerV = roundedCornerV;
 	}
 }
